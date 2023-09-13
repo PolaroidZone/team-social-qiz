935d2da260834190efb4504777b01d38576c40c7
Tsogang Mosweswe <tsogangamosweswe@gmail.com>
2023-09-13 15:21:20 +0200

    Update Register.tsx

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -5,13 +5,13 @@ interface RegisterProps {
   setPage: React.Dispatch<React.SetStateAction<string>>;
 }
 
-const Register: React.FC<RegisterProps> = ({setPage}: RegisterProps) => {
+const Register: React.FC<RegisterProps> = ({ setPage }: RegisterProps) => {
   return (
     <div className="Form-container">
       <div className="Register-header">
         <h1>Team social typescript</h1>
       </div>
-      <RegisterForm setPage={setPage}/>
+      <RegisterForm setPage={setPage} />
     </div>
   );
 };
