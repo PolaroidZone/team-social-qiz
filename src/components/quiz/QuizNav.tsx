9662179695b357d4ac197d7fbb9c7edcaa189939
Tsogang Mosweswe <tsogangamosweswe@gmail.com>
2023-09-18 13:48:31 +0200

    Setup API callling

--- src/components/quiz/QuizNav.tsx
+++ src/components/quiz/QuizNav.tsx
@@ -1,20 +1,34 @@
-import { SetStateAction } from "react";
+import { SetStateAction, useState } from "react";
+import axios from "axios";
 
 interface QuizNavProps {
-    setSelectedTopic:  React.Dispatch<SetStateAction<undefined>>;
+  setSelectedTopic: React.Dispatch<SetStateAction<undefined>>;
 }
 
-const QuizNav = ({setSelectedTopic}: QuizNavProps) => {
-    function handleBackClick(){
-        setSelectedTopic(undefined)
-    }
+const QuizNav = ({ setSelectedTopic }: QuizNavProps) => {
+  const [memes, setMemes] = useState([]);
+
+  axios({
+    method: "get",
+    url: "http://localhost:8000/memes",
+    responseType: "json",
+  }).then((response) => {
+    setMemes(response.data);
+  });
+
+  function handleBackClick() {
+    setSelectedTopic(undefined);
+  }
+
   return (
     <div className="paper-header">
       <div className="quiz-title">
         <h1>Team social quiz</h1>
       </div>
       <div className="quiz-nav">
-        <button className="quit-equiz" onClick={()=> handleBackClick()}>Back</button>
+        <button className="quit-equiz" onClick={() => handleBackClick()}>
+          Back
+        </button>
       </div>
     </div>
   );
